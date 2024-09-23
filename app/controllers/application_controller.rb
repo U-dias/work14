9c6d82d93122e3cf6b67e8955d2b13099b5d094a
U-dias <nomd_adniss@yahoo.co.jp>
2024-09-24 06:02:59 +0900

    アバター画像の変更を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:full_name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:full_name, :phone_number])
   end
 
   def after_sign_in_path_for(resource)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_22_145953) do
+ActiveRecord::Schema.define(version: 2024_09_22_161056) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2024_09_22_145953) do
     t.string "full_name"
     t.text "about"
     t.boolean "status", default: false
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

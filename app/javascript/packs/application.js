f29eed83df994bec8c53dbd02cc6ee1fe3ca20bd
U-dias <nomd_adniss@yahoo.co.jp>
2025-03-06 00:05:01 +0900

    削除前

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap"
+
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -17,3 +17,5 @@ environment.toWebpackConfig().merge({
         }
     }
 });
+
+environment.splitChunks();
